be01357c989fa4c67f5a2a1891fa628294b41cbc
Srikanth Sivashankaran <srikanth.sivashankaran@mckesson.com>
2021-10-20 18:37:48 -0400

    App v1.0

--- Explorer/app.R
+++ Explorer/app.R
@@ -1,8 +1,9 @@
 library(shiny)
 library(tidyverse)
+library(gmodels)
 
 # prepare the evals
-evals <- data.table::fread("wharton_reports_wharton-course-evaluations.csv",header=T)
+evals <- data.table::fread("./data/wharton_reports_wharton-course-evaluations.csv",header=T)
 availableElectives <- c("ACCT706","ACCT747","BEPP770","FNCE703","FNCE705","FNCE707","FNCE717","FNCE731","FNCE738","FNCE750","FNCE751","FNCE754","FNCE756","FNCE780","FNCE783","FNCE791","FNCE802","FNCE811","HCMG841","HCMG845","HCMG850","HCMG852","HCMG860","HCMG867","LGST642","LGST813","LGST815","MGMT624","MGMT671","MGMT701","MGMT721","MGMT731","MGMT748","MGMT794","MGMT801","MGMT802","MGMT804","MGMT806","MGMT809","MGMT811","MGMT812","MGMT871","MKTG711","MKTG712","MKTG721","MKTG739","MKTG747","MKTG754","MKTG777","MKTG778","MKTG852","OIDD613","OIDD614","OIDD615","OIDD636","OIDD643","OIDD653","OIDD654","OIDD662","OIDD667","OIDD673","REAL890","REAL891","STAT701","STAT722","ACCT897","BEPP763","BEPP773","LGST693","LGST806","MGMT690","MGMT793")
 evals <- evals %>%
   mutate(Course = substr(Section,1,7)) %>% 
@@ -10,34 +11,80 @@ evals <- evals %>%
   mutate(Year = zoo::as.yearmon(as.numeric(substr(Term,1,4)))) %>%
   mutate(Available = Course %in% availableElectives)
 evals <- evals[,c(16:19,1:15)]
-
+courseTitles <- evals %>%
+                    group_by(Course) %>%
+                    summarise(Title = max(Title))
 
 # Define UI ----
 ui <- fluidPage(
   titlePanel(h1("Wharton EMBA Course Eval Explorer")),
-  
   sidebarLayout(
     sidebarPanel(h1("Define your output"),
                  checkboxInput(inputId = "onlyCurrent",label = "Only consider WEMBA 47 West courses",value = T),
                  checkboxGroupInput("groupingVars",
                                     h3("Grouping Variables"),
-                                    choices = c("Course","Department","Instructor"),selected = 1),
+                                    choices = c("Course","Department","Instructor"),selected = c("Course","Department","Instructor")),
                  checkboxGroupInput("analysisVars",
-                                    h3("Analysis Variables"), choices = colnames(evals)[9:19],selected = 1),
-                 checkboxGroupInput("analyses",h3("Summary Functions"),choices = c("Mean","Min","Max","Most Recent","95ci min","95ci max"),selected=1),
-                 textOutput("onlyCurrent"),
-                 textOutput("groupingVars"),
+                                    h3("Analysis Variables"), choices = colnames(evals)[9:19],selected = "Overall Quality of Instructor (0=worst 4=best)"),
+                 checkboxGroupInput("analyses",h3("Summary Functions"),
+                                    choices = c("Mean","Min","Max","95ci"),
+                                    selected="Mean"),
                  ),
     mainPanel(DT::dataTableOutput("results"))
   )
 )
 
+ninetyfiveciLow <- function(values){
+  gmodels::ci(values)[2]
+}
+
+ninetyfiveciHigh <- function(values){
+  gmodels::ci(values)[3]
+}
+
 # Define server logic ----
 server <- function(input, output) {
-  output$onlyCurrent <- renderText({input$onlyCurrent})
-  output$groupingVars <- renderText({input$groupingVars})
-  results <- evals %>% dplyr::filter(Available == {input$onlyCurrent})
-  output$results <- DT::renderDataTable({results})
+  onlyCurrent <- reactive({input$onlyCurrent})
+  groupingVars <- reactive({input$groupingVars})
+  analyses <- reactive({input$analyses})
+  analysisVars <- reactive({input$analysisVars})
+  functions <- reactive({
+    na.omit(c(base::ifelse("Mean" %in% analyses(),"mean",NA),
+              base::ifelse("Min" %in% analyses(),"min",NA),
+              base::ifelse("Max" %in% analyses(), "max", NA),
+              base::ifelse("95ci" %in% analyses(),"ninetyfiveciLow",NA),
+              base::ifelse("95ci" %in% analyses(),"ninetyfiveciHigh",NA)
+              ))
+      # base::ifelse("95ci" %in% analyses(),c("ninetyfiveciLow","ninetyfiveciHigh"), NA),
+      #"n"))
+  })
+  analysis <- reactive({
+    evals %>%
+      {if(onlyCurrent()) filter(., Available == T) else . } %>%
+      group_by_at(groupingVars()) %>%    
+      summarise_at(.funs = functions(),.vars = analysisVars())
+  })
+  counts <- reactive({
+    evals %>%
+      group_by_at(groupingVars()) %>%
+      summarise(n=n())
+  })
+  withCounts <- reactive({
+    analysis() %>%
+      left_join(counts(),by=groupingVars())
+  })
+  results <- reactive({
+    if("Course" %in% groupingVars()){
+      withCounts() %>% 
+        left_join(courseTitles,by="Course")
+    }else{
+      withCounts()
+    }
+  })
+  output$onlyCurrent <- renderText(input$onlyCurrent)
+  output$groupingVars <- renderText(input$groupingVars)
+  output$results <- DT::renderDataTable(results())
+  output$functions <- renderText(functions())
 }
 
 # Run the app ----
--- sandbox.R
+++ sandbox.R
@@ -1,8 +1,30 @@
 library(tidyverse)
 evals <- data.table::fread("wharton_reports_wharton-course-evaluations.csv",header=T)
 availableElectives <- c("ACCT706","ACCT747","BEPP770","FNCE703","FNCE705","FNCE707","FNCE717","FNCE731","FNCE738","FNCE750","FNCE751","FNCE754","FNCE756","FNCE780","FNCE783","FNCE791","FNCE802","FNCE811","HCMG841","HCMG845","HCMG850","HCMG852","HCMG860","HCMG867","LGST642","LGST813","LGST815","MGMT624","MGMT671","MGMT701","MGMT721","MGMT731","MGMT748","MGMT794","MGMT801","MGMT802","MGMT804","MGMT806","MGMT809","MGMT811","MGMT812","MGMT871","MKTG711","MKTG712","MKTG721","MKTG739","MKTG747","MKTG754","MKTG777","MKTG778","MKTG852","OIDD613","OIDD614","OIDD615","OIDD636","OIDD643","OIDD653","OIDD654","OIDD662","OIDD667","OIDD673","REAL890","REAL891","STAT701","STAT722","ACCT897","BEPP763","BEPP773","LGST693","LGST806","MGMT690","MGMT793")
-evals %>%
+evals <- evals %>%
   mutate(Course = substr(Section,1,7)) %>% 
   mutate(Department = substr(Section,1,4)) %>%
-  mutate(Year = zoo::as.yearmon(as.numeric(substr(Term,1,4)))) 
+  mutate(Year = zoo::as.yearmon(as.numeric(substr(Term,1,4)))) %>%
+  mutate(Available = Course %in% availableElectives)
 evals <- evals[,c(16:19,1:15)]
+courseTitles <- evals %>%
+  group_by(Course) %>%
+  summarise(Title = max(Title))
+
+evals %>%
+  {if(T) filter(., Available == T) else . } %>%
+  group_by_at(c("Course"))    %>%
+  summarise_at(.funs = c("mean","max"),.vars = c("Overall Quality of Instructor (0=worst 4=best)","Rate the Difficulty of this Course (0=easiest 4=hardest)")) 
+
+groupingVars <- c("Course")
+functions <- c("mean","max")
+analysisVars = c("Overall Quality of Course (0=worst 4=best)")
+
+evals %>%
+  {if(F) filter(., Available == T) else . } %>%
+  group_by_at(groupingVars)    %>%
+  summarise_at(.funs = functions,.vars = analysisVars)
+
+evals %>%
+  group_by (Course) %>%
+  summarise(n()) %>% View()
